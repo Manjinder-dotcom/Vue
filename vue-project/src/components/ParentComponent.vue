db70a3b8cc44e8515bfd9baf7517145907611a7f
Manjinder <manjinder@xyrintech.com>
2022-12-15 13:47:28 +0530

    adds validators to props

--- vue-project/src/components/ParentComponent.vue
+++ vue-project/src/components/ParentComponent.vue
@@ -1,29 +1,42 @@
 <script setup>
 import { ref } from "vue";
+
 const props = defineProps({
-  name: { type: String, require: true },
-  phoneNumber: { type: String, require: true },
-  emailAddress: { type: String, require: true },
+  name: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  emailAddress: { type: String, required: true },
+  isFavorite: { type: String, required: false, default: '0', validator : function(value) {return value === '0' || value ==='1'} },
 });
 
-let detailsVisible = ref(true);
+const detailsVisible = ref(true);
 
-const friend = ref({
-  id: "Aman",
-  name: "AmanPreet",
-  phoneNumber: "5566778899",
-  emailAddress: "Aman@gmail.com",
-});
+// const friend = ref({
+//   id: "Aman",
+//   name: "AmanPreet",
+//   phoneNumber: "5566778899",
+//   emailAddress: "Aman@gmail.com",
+// });
+
+const friendisFavorite = ref('isFavorite')
 
 const toggleDetails = () => {
   detailsVisible.value = !detailsVisible.value;
 };
+
+const toggleFavorite = () => {
+  if(friendisFavorite.value === '1'){
+    friendisFavorite.value = '0'
+  }else {
+    friendisFavorite.value = '1'
+  }
+}
 </script>
 
 <template>
   <ul>
     <li>
-      <h2>{{ name }}</h2>
+      <h2>{{ name }} {{friendisFavorite === '1' ? '(Favorite)' : ''}} </h2>
+      <button @click="toggleFavorite" >Toggle Favorite</button>
       <button @click="toggleDetails()">
         {{ detailsVisible ? "Hide" : "Show" }} Details
       </button>

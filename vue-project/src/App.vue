9ff30d590602feb5767dc517281c5698729a2e52
Manjinder <mahimanjinder9@gmail.com>
2022-12-15 10:16:45 +0530

    initial commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+import ParentComponentVue from './components/ParentComponent.vue';
+</script>
+
+<template>
+ 
+  <ParentComponentVue/>
+   
+
+ 
+
+  
+</template>
